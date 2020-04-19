f138bd60f14468897e798907cdb1b612ed2f149d
小李子 <2366694349.@qq.com>
2020-04-19 20:02:15 +0800

    编写MVP框架以及网络框架

--- Home/src/main/java/com/ll/home/ui/act/HomeActivity.java
+++ Home/src/main/java/com/ll/home/ui/act/HomeActivity.java
@@ -5,31 +5,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.ll.common.router.RouterPath;
+import com.ll.core.ui.BaseActivity;
 import com.ll.home.R;
+import com.ll.home.contract.HomeContract;
+import com.ll.home.presenter.HomePresenter;
 
 @Route(path = RouterPath.HOME_ACTIVITY)
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity<HomeContract.HomePresenter> implements HomeContract.HomeView {
+
+
     private Button actHomeBtn;
+    private TextView actContentTv;
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-        initView();
+    protected void createPresenter() {
+        mPresenter = new HomePresenter(this);
     }
 
-    private void initView() {
-        actHomeBtn = (Button) findViewById(R.id.act_home_btn);
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initEvent() {
         actHomeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(HomeActivity.this, "欢迎来到HOME_ACTIVITY", Toast.LENGTH_SHORT).show();
+                mPresenter.getData();
             }
         });
+    }
+
+    @Override
+    protected void initView(Bundle savedInstanceState) {
+
+        actHomeBtn = (Button) findViewById(R.id.act_home_btn);
+        actContentTv = (TextView) findViewById(R.id.act_content_tv);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_home;
+    }
+
+    @Override
+    public void success(String string) {
+        showMsg("数据请求成功");
+        actContentTv.setText(string);
+    }
+
+    @Override
+    public void failed(Throwable throwable) {
+
     }
 }
